05d3b71dc2a648e1b64b069a1fc4589ff1e15fdd
DAPR-Dans <49371012+DAPR-Dans@users.noreply.github.com>
2019-04-07 23:54:54 +0100

    Update users.js

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 require('./util');
 
-<!--variable which creates new shema and stores username,password, id values  and creates string acces token -->
+/*variable which creates new shema and stores username,password, id values  and creates string acces token */
 var usersSchema = new Schema({
     user_name: {type: String},
     password: String,
@@ -12,12 +12,12 @@ var usersSchema = new Schema({
 });
 
  
-<!--function to generate hash-->
+/*function to generate hash*/
 usersSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
 };
 
-<!--function to check if password is valid-->
+/*function to check if password is valid*/
 usersSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
 };
